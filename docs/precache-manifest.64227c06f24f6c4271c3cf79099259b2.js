531b7c66eadb788d1a7e5c8ce7c6b68e4ee8045a
RichieAHB <richard.beddington@guardian.co.uk>
2018-11-23 19:20:50 +0000

    Build for GH pages

---
+++ docs/precache-manifest.64227c06f24f6c4271c3cf79099259b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbd8041b2a4ec8a6aa62",
+    "url": "/static/js/main.bbd8041b.chunk.js"
+  },
+  {
+    "revision": "667497788f0cda3b4787",
+    "url": "/static/js/1.66749778.chunk.js"
+  },
+  {
+    "revision": "3643d961285fac0925bdc1f5d3cf389a",
+    "url": "/index.html"
+  }
+];
