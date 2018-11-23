5278337a3435c72b051127b8294f705a2e67a89d
RichieAHB <richard.beddington@guardian.co.uk>
2018-11-23 19:28:33 +0000

    Updates

---
+++ precache-manifest.6f971b432d5b5f42280b2bbb80b391f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ccafb47b71de32502d6",
+    "url": "/redux-side-effects/static/js/runtime~main.3ccafb47.js"
+  },
+  {
+    "revision": "bbd8041b2a4ec8a6aa62",
+    "url": "/redux-side-effects/static/js/main.bbd8041b.chunk.js"
+  },
+  {
+    "revision": "667497788f0cda3b4787",
+    "url": "/redux-side-effects/static/js/1.66749778.chunk.js"
+  },
+  {
+    "revision": "6b8c5be2a379486c609212268a6169da",
+    "url": "/redux-side-effects/index.html"
+  }
+];
